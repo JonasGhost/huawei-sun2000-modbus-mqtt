3f72af73c7f7c016cad351ab542ddbdad52698b2
Tiago Ribeiro <tiago.err@outlook.com>
2022-04-09 09:51:17 +0100

    - Adapted the python file to the environment variables of the docker-compose.yml; - Joined the multiple values being published via MQTT; - Used list comprehension to simplify the code and join the values; - Added some more explicit logs; - Changed the MQTT topics;

--- huaweisolar.py
+++ huaweisolar.py
@@ -1,102 +1,99 @@
+import logging
 import time
-from huawei_solar import HuaweiSolar
 import huawei_solar
 import paho.mqtt.client
 import os
+import json
 
-import logging
 FORMAT = ('%(asctime)-15s %(threadName)-15s '
           '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
-inverter_ip = os.getenv('INVERTER_IP', '192.168.1.14')
-mqtt_host = os.getenv('MQTT_HOST', '192.168.1.15')
+inverter_ip = os.getenv('INVERTER_IP', '192.168.1.1')
+mqtt_host = os.getenv('MQTT_HOST', '192.168.1.1')
 
-inverter = huawei_solar.HuaweiSolar(inverter_ip, port=502, slave=1)
+inverter = huawei_solar.HuaweiSolar(inverter_ip, port=int(
+    os.getenv('INVERTER_PORT', "502")), slave=1)
 inverter._slave = 1
 inverter.wait = 1
 
-#vars = ['state_1','state_2', 'state_3', 'alarm_1', 'alarm_2', 'alarm_3', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage', 
-#'grid_current', 'day_active_power_peak', 'active_power', 'reactive_power', 
+# vars = ['state_1','state_2', 'state_3', 'alarm_1', 'alarm_2', 'alarm_3', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage',
+#'grid_current', 'day_active_power_peak', 'active_power', 'reactive_power',
 #'grid_frequency', 'efficiency', 'internal_temperature', 'insulation_resistance', 'device_status', 'fault_code', 'startup_time', 'shutdown_time', 'accumulated_yield_energy',
-#'daily_yield_energy', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power', 
-#'grid_exported_energy', 'grid_accumulated_energy']
+#'daily_yield_energy', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power',
+# 'grid_exported_energy', 'grid_accumulated_energy']
 
-#vars = ['alarm_1', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage', 
-#'grid_current', 'day_active_power_peak', 'active_power', 'reactive_power', 
+# vars = ['alarm_1', 'pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage',
+#'grid_current', 'day_active_power_peak', 'active_power', 'reactive_power',
 #'grid_frequency', 'efficiency', 'internal_temperature', 'insulation_resistance', 'device_status', 'fault_code', 'startup_time', 'shutdown_time', 'accumulated_yield_energy',
-#'daily_yield_energy', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power', 
-#'grid_exported_energy', 'grid_accumulated_energy']
+#'daily_yield_energy', 'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power',
+# 'grid_exported_energy', 'grid_accumulated_energy']
+
+
+def try_modBus_variable(variable):
+    try:
+        result = inverter.get(variable)
+        return {'value': result.value, 'unit': result.unit}
+    except:
+        log.warning(f"❌ Failed to get {variable}!")
+        return {'value': -1, 'unit': "?"}
 
 
 def modbusAccess():
 
-    vars_inmediate = ['pv_01_voltage', 'pv_01_current', 'pv_02_voltage','pv_02_current', 'input_power', 'grid_voltage', 
-    'grid_current', 'active_power', 
-    'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power']
+    vars_inmediate = ['pv_01_voltage', 'pv_01_current', 'pv_02_voltage', 'pv_02_current', 'input_power', 'grid_voltage',
+                      'grid_current', 'active_power',
+                      'grid_A_voltage', 'active_grid_A_current', 'power_meter_active_power']
 
     vars = ['day_active_power_peak', 'efficiency', 'internal_temperature', 'insulation_resistance', 'device_status', 'fault_code', 'accumulated_yield_energy',
-    'daily_yield_energy', 'grid_exported_energy', 'grid_accumulated_energy']
+            'daily_yield_energy', 'grid_exported_energy', 'grid_accumulated_energy']
 
     cont = 0
     while True:
-
-        for i in vars_inmediate:
-            try:
-                mid = inverter.get(i)
-                log.debug(i)
-                log.debug(mid)
-                log.debug("---")
-
-                clientMQTT.publish(topic="emon/NodeHuawei/"+i, payload= str(mid.value), qos=1, retain=False)
-            except:
-                pass
+        immediate_results = {var: try_modBus_variable(
+            var) for var in vars_inmediate}
+        clientMQTT.publish(topic="huawei/node/immediate",
+                           payload=json.dumps(immediate_results), qos=1, retain=False)
+        log.info('🚀 Publishing immediate results...')
 
         if(cont > 5):
-            for i in vars:
-                try:
-                    mid = inverter.get(i)
-                    log.debug(i)
-                    log.debug(mid)
-                    log.debug("---")
-
-                    clientMQTT.publish(topic="emon/NodeHuawei/"+i, payload= str(mid.value), qos=1, retain=False)
-                except:
-                    pass
+            results = {var: try_modBus_variable(var) for var in vars}
+            clientMQTT.publish(topic="huawei/node/cumulative",
+                               payload=json.dumps(results), qos=1, retain=False)
+            log.info('🚀 Publishing cumulative results...')
 
             cont = 0
 
-        cont = cont + 1   
+        cont += 1
         time.sleep(1)
 
 
-
-
-
 def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        client.connected_flag=True #set flag
+    if rc == 0:
+        client.connected_flag = True  # set flag
         log.info("MQTT OK!")
     else:
-        log.info("MQTT FAILURE. ERROR CODE: %s",rc)
+        log.info("MQTT FAILURE. ERROR CODE: %s", rc)
 
 
-paho.mqtt.client.Client.connected_flag=False#create flag in class
-broker_port = 1883
+paho.mqtt.client.Client.connected_flag = False  # create flag in class
+broker_port = int(os.getenv("MQTT_PORT", "1883"))
 
 clientMQTT = paho.mqtt.client.Client()
-clientMQTT.on_connect=on_connect #bind call back function
+clientMQTT.on_connect = on_connect  # bind call back function
 clientMQTT.loop_start()
-log.info("Connecting to MQTT broker: %s ",mqtt_host)
-clientMQTT.username_pw_set(username="",password="")
-clientMQTT.connect(mqtt_host, broker_port) #connect to broker
-while not clientMQTT.connected_flag: #wait in loop
+log.info("Connecting to MQTT broker: %s ", mqtt_host)
+clientMQTT.username_pw_set(username=os.getenv('MQTT_USER', 'user'),
+                           password=os.getenv('MQTT_PASS', 'password'))
+clientMQTT.connect(mqtt_host, broker_port)  # connect to broker
+while not clientMQTT.connected_flag:  # wait in loop
     log.info("...")
 time.sleep(1)
 log.info("START MODBUS...")
 
-modbusAccess()
-
-clientMQTT.loop_stop()
+try:
+    modbusAccess()
+except:
+    clientMQTT.loop_stop()
